6f9534545cb953ad5d952d7b270a134e1f3c1006
Sidharth Mohanty <sidmohanty11@gmail.com>
2022-03-15 15:36:14 +0530

    using styles

--- using-webpack/src/index.js
+++ using-webpack/src/index.js
@@ -1,9 +1,10 @@
 import React from "react";
+import styles from "./index.module.css";
 
 const App = () => {
   return (
     <div>
-      <h1>Hello!!!</h1>
+      <h1 className={styles.hello}>Hello!!!</h1>
     </div>
   );
 };
